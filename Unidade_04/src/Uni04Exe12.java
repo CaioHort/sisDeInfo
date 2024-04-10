6b981134e2319dc549e973bfd7f65f4a1d3553ea
Caio Henrique Hort <66917896+CaioHort@users.noreply.github.com>
2024-04-10 18:55:38 -0300

    Update Uni04Exe12.java

--- Unidade_04/src/Uni04Exe12.java
+++ Unidade_04/src/Uni04Exe12.java
@@ -11,24 +11,23 @@ public class Uni04Exe12 {
         int lado2 = input.nextInt();
         System.out.print("Informe lado 3...:");
         int lado3 = input.nextInt();
-        
-        if (lado1 < lado2 + lado3) && 
-        (lado2 < lado2 + lado3) &&
-        (lado3 < lado1 + lado2) {
-                if ((lado1 == lado2) && (lado1 == lado3)) {
-                    System.out.println("Triangulo equilatero");
-                } else if ((lado1 == lado2) ||
-                (lado 2 == lado3) ||
-                (lado3 == lado1)) {
-                    System.out.println("Triangulo isóseles");
-                } else {
-                    System.out.println("Triangulo escaleno");
-                }
+
+        if ((lado1 < lado2 + lado3) &&
+                (lado2 < lado2 + lado3) &&
+                (lado3 < lado1 + lado2)) {
+            if ((lado1 == lado2) && (lado1 == lado3)) {
+                System.out.println("Triangulo equilatero");
+            } else if ((lado1 == lado2) ||
+                    (lado2 == lado3) ||
+                    (lado3 == lado1)) {
+                System.out.println("Triangulo isóseles");
+            } else {
+                System.out.println("Triangulo escaleno");
+            }
         } else {
             System.out.println("Não forma um triangulo");
         }
 
-        
         input.close();
     }
 }
